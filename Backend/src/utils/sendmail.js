c06d86aa46b3854f65d66d3a42bc22827c171c72
sumitn2424-debug <sumitn2424@gmail.com>
2026-04-09 23:21:19 +0530

    solving smtp using brevo

--- Backend/src/utils/sendmail.js
+++ Backend/src/utils/sendmail.js
@@ -1,32 +1,62 @@
-// src/utils/sendMail.js
-const nodemailer = require("nodemailer");
+// // src/utils/sendMail.js
+// const nodemailer = require("nodemailer");
+
+// const sendMail = async (to, subject, text) => {
+//   if (!process.env.SMTP_EMAIL || !process.env.SMTP_APP_PASSWORD) {
+//     console.error("❌ SMTP_EMAIL or SMTP_APP_PASSWORD missing in .env");
+//     return { success: false, error: "Email config missing" };
+//   }
+
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       port: 587 || 465,
+//       host: "smtp.gmail.com",
+//       // service: "gmail",
+//       secure: true,
+//       auth: {
+//         user: process.env.SMTP_EMAIL,
+//         pass: process.env.SMTP_APP_PASSWORD,
+//       },
+      
+//       family: 4
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: `"UniSync" <${process.env.SMTP_EMAIL}>`,
+//       to,
+//       subject,
+//       text,
+//     });
+
+//     console.log("🚀 Email sent:", info.messageId);
+//     return { success: true, data: info };
+//   } catch (err) {
+//     console.error("❌ Error sending email:", err.message);
+//     return { success: false, error: err.message };
+//   }
+// };
+
+// module.exports = sendMail;
+
+
+const axios = require("axios");
 
 const sendMail = async (to, subject, text) => {
-  if (!process.env.SMTP_EMAIL || !process.env.SMTP_APP_PASSWORD) {
-    console.error("❌ SMTP_EMAIL or SMTP_APP_PASSWORD missing in .env");
-    return { success: false, error: "Email config missing" };
-  }
-
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_APP_PASSWORD,
+    const res = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: { email: "demod2223@gmail.com", name: "Unisync" },
+        to: [{ email: to }],
+        subject,
+        textContent: text,
       },
-    });
-
-    const info = await transporter.sendMail({
-      from: `"UniSync" <${process.env.SMTP_EMAIL}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("🚀 Email sent:", info.messageId);
-    return { success: true, data: info };
+      {
+        headers: { "api-key": process.env.BREVO_API_KEY }
+      }
+    );
+    return { success: true, data: res.data };
   } catch (err) {
-    console.error("❌ Error sending email:", err.message);
     return { success: false, error: err.message };
   }
 };
